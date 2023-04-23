f43bae86ecd3acc8aaf3b6f77c6c0a1c2809fbb7
oguzcicek <soaoflisa@hotmail.com>
2023-04-23 21:05:52 +0300

    cors add website manually

--- app/post/[id]/add-comment.jsx
+++ app/post/[id]/add-comment.jsx
@@ -8,13 +8,10 @@ export default function AddComment( { postId } ){
     const submitComment = (e) => {
         e.preventDefault();
       //  fetch(`http://localhost:3001/api/v1/posts/${postId}/post_comments`, {
-        fetch(`http://127.0.0.1:3001/api/v1/posts/${postId}/post_comments`, {
+        fetch(`http://admin.clubgamefi.com/api/v1/posts/${postId}/post_comments`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
             },
             body: JSON.stringify(form)
         }).then((res) => {
