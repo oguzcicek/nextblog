f0c6c8b7d24dfe5e7215ffd7299d7ba15066341b
oguzcicek <soaoflisa@hotmail.com>
2023-05-01 12:53:18 +0300

    analytics code v1

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../assets/styles/global.css'
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
@@ -10,6 +11,20 @@ export default function RootLayout({ children }) {
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+
+      <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6ED6SLF9ZR"
+          strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6ED6SLF9ZR');
+        `}
+      </Script>
       <body>
       <Header />
       {children}
