eac82f923770cc96a766a9d0202b537ef03ef14a
oguzcicek <soaoflisa@hotmail.com>
2023-11-11 22:41:31 +0300

    A

--- app/post/[id]/add-comment.jsx
+++ app/post/[id]/add-comment.jsx
@@ -7,15 +7,13 @@ export default function AddComment( { postId } ){
 
     const submitComment = (e) => {
         e.preventDefault();
-        headers.append('Access-Control-Allow-Origin', 'http://localhost:3001');
-        headers.append('Access-Control-Allow-Credentials', 'true');
      fetch(`http://127.0.0.1:3001/api/v1/posts/${postId}/post_comments`, {
      // fetch(`http://admin.clubgamefi.com/api/v1/posts/${postId}/post_comments`, {
             method: "POST",
             headers: {
                 'Accept': 'application/json',
                 "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Origin": "http://localhost:3001",
                 "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
                 "Access-Control-Allow-Headers":
                 "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
