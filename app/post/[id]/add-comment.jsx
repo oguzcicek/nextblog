36159ed0ad42472882d3a4221ba8056d68a95027
oguzcicek <soaoflisa@hotmail.com>
2023-11-11 16:16:16 +0300

    A

--- app/post/[id]/add-comment.jsx
+++ app/post/[id]/add-comment.jsx
@@ -7,7 +7,7 @@ export default function AddComment( { postId } ){
 
     const submitComment = (e) => {
         e.preventDefault();
-        fetch(`http://127.0.0.1:3001/api/v1/posts/${postId}/post_comments`, {
+        fetch(`localhost:3001/api/v1/posts/${postId}/post_comments`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
